57245503ced4f5133f52b6d2a7bf723da9f88480
minhnguyen268 <165470790+minhnguyen268@users.noreply.github.com>
2025-04-07 12:04:35 +0700

    Update ListUserBank.js

--- front-end/components/admin/users/ListUserBank.js
+++ front-end/components/admin/users/ListUserBank.js
@@ -1,10 +1,19 @@
 import useGetListUserBank from "@/hooks/admin/useGetListUserBank";
 import { convertDateTime } from "@/utils/convertTime";
-import { Box } from "@mui/material";
+import { Box, Dialog, DialogActions, DialogContent, DialogTitle, Button } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
+import EditIcon from "@mui/icons-material/Edit";
+import { useState } from "react";
+import FormBank from "@/components/admin/settings/Bank/FormBank";
+import DeleteIcon from "@mui/icons-material/Delete";
+import BankService from "@/services/admin/BankService";
+import { toast } from "react-toastify";
 
 const ListUserBank = ({ ID }) => {
-  const { data: dataQuery, isLoading } = useGetListUserBank({ userId: ID });
+  const { data: dataQuery, isLoading, refetch } = useGetListUserBank({ userId: ID });
+  const [selectedBank, setSelectedBank] = useState(null);
+  const [deleteBank, setDeleteBank] = useState(null);
+  const [isLoadingDelete, setIsLoadingDelete] = useState(false);
 
   const GridRowsProp =
     dataQuery?.map((item, i) => ({
@@ -28,7 +37,41 @@ const ListUserBank = ({ ID }) => {
     { field: "tenChuTaiKhoan", headerName: "Chủ tài khoản", width: 250 },
 
     { field: "createdAt", headerName: "Thời gian tạo", width: 250 },
+    {
+      field: "action",
+      headerName: "Thao tác",
+      type: "actions",
+      width: 150,
+      getActions: (params) => [
+        <div sx={{ padding: "0px 10px", backgroundColor: "transparent !important" }}>
+          <EditIcon sx={{ marginRight: "15px" }} onClick={() => setSelectedBank(params.row)} />
+          <DeleteIcon sx={{ color: "red" }} onClick={() => setDeleteBank(params.row)} />
+        </div>,
+      ],
+    },
   ];
+
+  const handleUpdate = async (data) => {
+    const res = await BankService.updateBankUser({
+      id: selectedBank.id,
+      data,
+    });
+
+    refetch();
+
+    return res;
+  };
+
+  const handleDelete = async () => {
+    setIsLoadingDelete(true);
+    await BankService.deleteBankUser({
+      id: deleteBank.id,
+    });
+    setIsLoadingDelete(false);
+    setDeleteBank(null);
+    refetch();
+    toast.success("Xoá ngân hàng thành công");
+  };
 
   return (
     <>
@@ -90,6 +133,54 @@ const ListUserBank = ({ ID }) => {
           }}
         />
       </Box>
+
+      <Dialog
+        open={selectedBank}
+        onClose={() => {
+          setSelectedBank(null);
+        }}
+        fullWidth
+      >
+        <DialogTitle>Thay đổi thông tin ngân hàng</DialogTitle>
+        <DialogContent>
+          <FormBank
+            data={selectedBank}
+            handleCancel={() => {
+              setSelectedBank(null);
+            }}
+            handleOnSubmit={handleUpdate}
+            isEditingUserBank
+          />
+        </DialogContent>
+      </Dialog>
+
+      <Dialog
+        open={deleteBank}
+        onClose={() => {
+          setDeleteBank(null);
+        }}
+        fullWidth
+      >
+        <DialogTitle>Bạn có chắc chắn muốn xoá ngân hàng này không?</DialogTitle>
+        <DialogActions>
+          <Button
+            onClick={() => {
+              setDeleteBank(null);
+            }}
+            disabled={isLoadingDelete}
+          >
+            Huỷ
+          </Button>
+          <Button
+            onClick={() => {
+              handleDelete();
+            }}
+            loading={isLoadingDelete}
+          >
+            Xoá
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 };
