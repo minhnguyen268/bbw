ee8edd57825cb26f3ce640fa4be005656d1953a5
minhnguyen268 <165470790+minhnguyen268@users.noreply.github.com>
2025-04-07 11:54:39 +0700

    Update nguoidung.controller.js

--- back-end/controllers/nguoidung.controller.js
+++ back-end/controllers/nguoidung.controller.js
@@ -1,253 +1,201 @@
-const NguoiDung = require("../models/NguoiDung");
-const Setting = require("../models/Setting");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const { NotFoundError, UnauthorizedError, BadRequestError } = require("../utils/app_error");
-const catchAsync = require("../utils/catch_async");
-const { OkResponse, CreatedResponse } = require("../utils/successResponse");
-const { selectFields, unSelectFields } = require("../utils/selectFieldsObject");
-const ms = require("ms");
-const { signToken } = require("../utils/signToken");
-const { JWT_SECRET_KEY } = require("../configs/jwt.config");
-const { USER_ROLE } = require("../configs/user.config");
-class NguoiDungController {
-  static getDetailedUser = catchAsync(async (req, res, next) => {
-    const { taiKhoan } = req.user;
-    const user = await NguoiDung.findOne({ taiKhoan }).select("-matKhau -__v -refreshToken").lean();
-    if (!user) {
-      throw new NotFoundError(`Không tồn tại tài khoản: ${taiKhoan}`);
-    }
+"use strict";
+
+const catchAsync = require("../../utils/catch_async");
+const _ = require("lodash");
+const { OkResponse, CreatedResponse } = require("../../utils/successResponse");
+
+const { BadRequestError, UnauthorizedError } = require("../../utils/app_error");
+const HeThong = require("../../models/HeThong");
+const listBank = require("../../configs/listBank");
+const LienKetNganHang = require("../../models/LienKetNganHang");
+
+class NganHangAdminController {
+  static getDanhSachNganHang = catchAsync(async (req, res, next) => {
+    let results = await HeThong.findOne({
+      systemID: 1,
+    }).select("danhSachNganHang");
     return new OkResponse({
-      data: user,
+      data: results?.danhSachNganHang ?? [],
     }).send(res);
   });
-  static refreshToken = catchAsync(async (req, res, next) => {
-    const { taiKhoan } = req.user;
-    const { refreshToken } = req.body;
-    if (!refreshToken) {
+
+  static createNganHang = catchAsync(async (req, res, next) => {
+    const { soTaiKhoan, tenChuTaiKhoan, code } = req.body;
+    if (!code || !soTaiKhoan || !tenChuTaiKhoan) {
       throw new UnauthorizedError("Vui lòng nhập đầy đủ thông tin");
     }
-    try {
-      const checkRefreshTokenValid = await NguoiDung.findOne({ refreshToken: { $in: refreshToken }, taiKhoan });
-      if (!checkRefreshTokenValid) {
-        return next(new BadRequestError("Refresh token không tồn tại"));
-      }
-      const decodeRefreshToken = jwt.verify(refreshToken, JWT_SECRET_KEY);
-
-      // Tạo mới token
-
-      const newToken = signToken({
-        taiKhoan: checkRefreshTokenValid.taiKhoan,
-        role: checkRefreshTokenValid.role,
-        id: checkRefreshTokenValid._id,
-      });
-      // Update database
-      await Promise.all([
-        NguoiDung.findOneAndUpdate(
-          { taiKhoan },
-          {
-            $pull: { refreshToken: refreshToken },
-          }
-        ),
-        NguoiDung.findOneAndUpdate(
-          { taiKhoan },
-          {
-            $push: { refreshTokenUsed: refreshToken, refreshToken: newToken.refreshToken },
-          }
-        ),
-      ]);
-
-      return new OkResponse({
-        data: newToken,
-      }).send(res);
-    } catch (err) {
-      console.log(err);
-      throw new BadRequestError("Refresh token đã hết hạn hoặc không tồn tại!");
+    const findBankByCode = listBank.find((item) => item.code === code);
+    if (!findBankByCode) {
+      throw new BadRequestError("Mã code ngân hàng không hợp lệ");
     }
-  });
+    const { logo: image, shortName, name: tenBank } = findBankByCode;
 
-  static createUser = catchAsync(async (req, res, next) => {
-    const { taiKhoan, matKhau, nhapLaiMatKhau, soDienThoai, maGioiThieu } = req.body;
-
-    const settingData = await Setting.findOne({}).lean();
-    // console.log(settingData.maGioiThieu);
-    // console.log(maGioiThieu);
-    if (settingData.maGioiThieu !== maGioiThieu) {
-      throw new BadRequestError("Mã giới thiệu không đúng");
-    }
-
-    if (!taiKhoan || !matKhau || !nhapLaiMatKhau) {
-      throw new UnauthorizedError("Vui lòng nhập đầy đủ thông tin");
-    }
-    const checkUserExist = await NguoiDung.findOne({
-      taiKhoan,
+    const findNganHang = await HeThong.findOne({
+      systemID: 1,
+      danhSachNganHang: {
+        $elemMatch: {
+          code: code,
+        },
+      },
     });
-    if (checkUserExist) {
-      throw new BadRequestError("Nguời dùng đã tồn tại");
+    if (findNganHang) {
+      throw new BadRequestError("Đã tồn tại ngân hàng này");
     }
-    let result;
-    const countUser = await NguoiDung.countDocuments({});
-    if (countUser === 0) {
-      result = await NguoiDung.create({
-        taiKhoan,
-        matKhau,
-        nhapLaiMatKhau,
-        soDienThoai,
-        role: USER_ROLE.ADMIN,
-      });
-    } else {
-      result = await NguoiDung.create({
-        taiKhoan,
-        matKhau,
-        nhapLaiMatKhau,
-        soDienThoai,
-      });
-    }
-    return new CreatedResponse({
-      data: result,
-      message: "Đăng ký tài khoản thành công. Đang tiến hành đăng nhập",
-    }).send(res);
-  });
-
-  static signInUser = catchAsync(async (req, res, next) => {
-    const { taiKhoan, matKhau } = req.body;
-    if (!taiKhoan || !matKhau) {
-      throw new UnauthorizedError("Vui lòng nhập đầy đủ thông tin");
-    }
-    const user = await NguoiDung.findOne({
-      taiKhoan,
-    }).lean();
-    if (!user) {
-      throw new BadRequestError("Nguời dùng không tồn tại");
-    }
-    const authPassword = await bcrypt.compare(matKhau, user.matKhau);
-    if (!authPassword) {
-      throw new BadRequestError("Mật khẩu không chính xác");
-    }
-    // Sign token
-
-    const { accessToken, refreshToken, expireAccessToken } = signToken({
-      taiKhoan: user.taiKhoan,
-      role: user.role,
-      id: user._id,
-    });
-
-    // Update refresh token DB
-    await NguoiDung.findOneAndUpdate(
+    await HeThong.updateOne(
       {
-        taiKhoan,
+        systemID: 1,
       },
       {
-        $push: { refreshToken: refreshToken },
-      }
-    );
-    return new OkResponse({
-      data: {
-        data: unSelectFields({ fields: ["matKhau", "__v", "refreshToken", "refreshTokenUsed"], object: user }),
-        accessToken,
-        refreshToken,
-        expireAccessToken,
-      },
-      message: "Đăng nhập thành công",
-    }).send(res);
-  });
-  static signOutUser = catchAsync(async (req, res, next) => {
-    const { refreshToken, taiKhoan } = req.body;
-
-    await NguoiDung.findOneAndUpdate(
-      {
-        taiKhoan,
-      },
-      {
-        $pull: {
-          refreshToken: refreshToken,
+        $push: {
+          danhSachNganHang: {
+            shortName,
+            tenBank,
+            image,
+            code,
+            soTaiKhoan,
+            tenChuTaiKhoan,
+          },
         },
       }
     );
 
-    return new OkResponse({
-      message: "Đăng xuất thành công",
+    return new CreatedResponse({
+      message: "Tạo thành công",
     }).send(res);
   });
-  static changePassword = catchAsync(async (req, res, next) => {
-    const { taiKhoan, matKhau } = req.user;
-    const { currentPassword, newPassword } = req.body;
-    if (!newPassword || !currentPassword) {
-      throw new BadRequestError("Vui lòng nhập đầy đủ dữ liệu");
-    }
-    if (newPassword === currentPassword) {
-      throw new BadRequestError("Mật khẩu mới không được trùng với mật khẩu hiện tại");
-    }
-
-    const authPassword = await bcrypt.compare(currentPassword, matKhau);
-    if (!authPassword) {
-      throw new BadRequestError("Mật khẩu hiện tại không chính xác");
-    }
-    const hashNewPassword = await bcrypt.hash(newPassword, 12);
-
-    // Update user
-    await NguoiDung.findOneAndUpdate(
+  static getChiTietNganHang = catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    let result = await HeThong.findOne(
       {
-        taiKhoan,
+        systemID: 1,
+        danhSachNganHang: {
+          $elemMatch: {
+            _id: id,
+          },
+        },
       },
       {
-        refreshToken: [],
-        matKhau: hashNewPassword,
+        "danhSachNganHang.$": 1,
+      }
+    );
+    if (!result) {
+      throw new BadRequestError("Không tìm thấy ngân hàng");
+    }
+    return new OkResponse({
+      data: result?.danhSachNganHang?.[0],
+    }).send(res);
+  });
+
+  static editNganHang = catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const { soTaiKhoan, tenChuTaiKhoan, code, status } = req.body;
+    const findBankByCode = listBank.find((item) => item.code === code);
+    if (!findBankByCode) {
+      throw new BadRequestError("Mã code ngân hàng không hợp lệ");
+    }
+    const { logo: image, shortName, name: tenBank } = findBankByCode;
+
+    const result = await HeThong.findOneAndUpdate(
+      { systemID: 1, "danhSachNganHang._id": id },
+      {
+        $set: {
+          "danhSachNganHang.$.code": code,
+          "danhSachNganHang.$.shortName": shortName,
+          "danhSachNganHang.$.tenBank": tenBank,
+          "danhSachNganHang.$.image": image,
+          "danhSachNganHang.$.soTaiKhoan": soTaiKhoan,
+          "danhSachNganHang.$.tenChuTaiKhoan": tenChuTaiKhoan,
+          "danhSachNganHang.$.status": !!status,
+        },
+      },
+      {
+        new: false,
+        projection: {
+          "danhSachNganHang.$": 1,
+        },
       }
     );
 
+    if (!result) {
+      throw new BadRequestError("Không tìm thấy ngân hàng");
+    }
+
     return new OkResponse({
-      message: "Đổi mật khẩu thành công",
+      message: "Edit thành công",
     }).send(res);
   });
-  static changePhone = catchAsync(async (req, res, next) => {
-    const { taiKhoan } = req.user;
-    const { phone } = req.body;
 
-    // Update user
-    await NguoiDung.findOneAndUpdate(
+  static deleteNganHang = catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const result = await HeThong.findOneAndUpdate(
       {
-        taiKhoan,
+        systemID: 1,
+        "danhSachNganHang._id": id,
       },
       {
-        soDienThoai: phone,
-      }
-    );
-    return new OkResponse({
-      message: "Đổi số điện thoại thành công",
-    }).send(res);
-  });
-  static updatePasswordWithdraw = catchAsync(async (req, res, next) => {
-    const { taiKhoan, matKhauRutTien: matKhauRutTienUser } = req.user;
-    const { matKhauRutTien } = req.body;
-
-    if (matKhauRutTienUser) {
-      throw new BadRequestError("Bạn đã cập nhật mật khẩu rút tiền rồi");
-    }
-    if (!matKhauRutTien) {
-      throw new BadRequestError("Vui lòng nhập đầy đủ dữ liệu");
-    }
-    const hashNewPassword = await bcrypt.hash(matKhauRutTien, 12);
-
-    // Update user
-    await NguoiDung.findOneAndUpdate(
-      {
-        taiKhoan,
+        $pull: {
+          danhSachNganHang: {
+            _id: id,
+          },
+        },
       },
       {
-        matKhauRutTien: hashNewPassword,
+        new: false,
+        projection: {
+          "danhSachNganHang.$": 1,
+        },
       }
     );
+    if (!result) {
+      throw new BadRequestError("Không tìm thấy ngân hàng");
+    }
     return new OkResponse({
-      message: "Cập nhật mật khẩu rút tiền thành công",
+      message: "Xóa thành công",
     }).send(res);
   });
-  static checkHasWithdrawPassword = catchAsync(async (req, res, next) => {
-    const { matKhauRutTien } = req.user;
+
+  static editNganHangUser = catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const { soTaiKhoan } = req.body;
+
+    const result = await LienKetNganHang.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: {
+          soTaiKhoan,
+        },
+      },
+      {
+        new: false,
+        projection: {
+          _id: 1,
+        },
+      }
+    );
+
+    if (!result) {
+      throw new BadRequestError("Không tìm thấy ngân hàng");
+    }
+
     return new OkResponse({
-      data: !!matKhauRutTien,
+      message: "Edit thành công",
+    }).send(res);
+  });
+
+  static deleteNganHangUser = catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const result = await LienKetNganHang.deleteOne({
+      _id: id,
+    });
+
+    if (!result) {
+      throw new BadRequestError("Không tìm thấy ngân hàng");
+    }
+
+    return new OkResponse({
+      message: "Xoá thành công",
     }).send(res);
   });
 }
 
-module.exports = NguoiDungController;
+module.exports = NganHangAdminController;
